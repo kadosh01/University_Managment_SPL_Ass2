e2e48e8547ae473873b6a03a849b30c4de91a58d
avabe <avabe@post.bgu.ac.il>
2017-12-18 00:08:39 +0200

    00:08

--- src/main/java/bgu/spl/a2/sim/actions/AddStudent.java
+++ src/main/java/bgu/spl/a2/sim/actions/AddStudent.java
@@ -1,8 +1,19 @@
 package bgu.spl.a2.sim.actions;
 
 import bgu.spl.a2.Action;
+import bgu.spl.a2.sim.privateStates.DepartmentPrivateState;
+import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 
-public class AddStudent {
+public class AddStudent extends Action<Boolean>{
 
+    public AddStudent(String studentName){
+        _actionName= studentName;
+    }
 
+    @Override
+    protected void start() {
+        StudentPrivateState sps= new StudentPrivateState();
+        sendMessage(null, _actionName, _privateState);
+        //((DepartmentPrivateState)_privateState)
+    }
 }
