b92d2e8ada48ddb35a3adf9b88b5a936e16c6aab
Joseph <kadosh01@gmail.com>
2017-12-18 00:06:52 +0200

    00:06

---
+++ src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
@@ -0,0 +1,13 @@
+package bgu.spl.a2.sim.actions;
+
+import bgu.spl.a2.Action;
+
+/**
+ * Created by Joseph on 17/12/2017.
+ */
+public class ParticipatingInCourse extends Action<Boolean> {
+    @Override
+    protected void start() {
+
+    }
+}
