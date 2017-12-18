55d52d709c6b2ce0d94c2413e1181f11d927706c
Joseph <kadosh01@gmail.com>
2017-12-18 22:11:34 +0200

    22:11 18.12

--- src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
+++ src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
@@ -1,6 +1,8 @@
 package bgu.spl.a2.sim.actions;
 
 import bgu.spl.a2.Action;
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
+import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -21,13 +23,29 @@ public class ParticipatingInCourse extends Action<Boolean> {
 
     @Override
     protected void start() {
-        List<Action<Boolean>> actions = new LinkedList<>();
-        actions.add(new Action<Boolean>() {
-            @Override
-            protected void start() {
+        if (((CoursePrivateState) _privateState).inc()) {
+            List<Action<Boolean>> actions = new LinkedList<>();
+            actions.add(new Action<Boolean>() {
+                @Override
+                protected void start() {
+                    StudentPrivateState sps=(StudentPrivateState) _pool.getActors().get(_studentId);
+                    sps.getGrades().put(_courseName,_grade);
+                    complete(true);
+                }
+            });
+            sendMessage(actions.get(0),_studentId,new StudentPrivateState());
+            then(actions,()->{
+                if(actions.get(0).getResult().get()){
+                    ((CoursePrivateState) _privateState).addStudent(_studentId);
+                    complete(true);
+                }
+                else {
+                    ((CoursePrivateState) _privateState).dec();
+                    complete(false);
+                }
+            });
+        }//END IF
 
-            }
-        });
     }
 
 }
