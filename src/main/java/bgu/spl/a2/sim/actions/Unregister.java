f97f18666848ff4f591a14ee99065e1d65f64be1
avabe <avabe@post.bgu.ac.il>
2017-12-18 22:30:33 +0200

    22:28

--- src/main/java/bgu/spl/a2/sim/actions/Unregister.java
+++ src/main/java/bgu/spl/a2/sim/actions/Unregister.java
@@ -1,8 +1,45 @@
 package bgu.spl.a2.sim.actions;
 
 import bgu.spl.a2.Action;
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
+import bgu.spl.a2.sim.privateStates.StudentPrivateState;
 
-public class Unregister {
+import java.util.LinkedList;
+import java.util.List;
 
+public class Unregister extends Action<Boolean>{
+    protected String _studentId;
 
+    public Unregister(String studentId, String courseName){
+        setActionName("Unregister");
+        _actorID= courseName;
+        _studentId= studentId;
+    }
+
+    @Override
+    protected void start() {
+        if(((CoursePrivateState)_privateState).getRegStudents().contains(_studentId)){
+            ((CoursePrivateState)_privateState).dec();
+            List<Action<Boolean>> actions= new LinkedList<>();
+            actions.add(new Action<Boolean>() {
+                @Override
+                protected void start() {
+                    StudentPrivateState sps= (StudentPrivateState)_pool.getActors().get(_studentId);
+                    sps.getGrades().remove(_actorID);
+                    complete(true);
+                }
+            });
+            sendMessage(actions.get(0), _studentId, new StudentPrivateState());
+            then(actions, ()->{
+               if(actions.get(0).getResult().get()){
+                   ((CoursePrivateState)_privateState).removeStudent(_studentId);
+                   complete(true);
+               }
+               else{
+                   ((CoursePrivateState)_privateState).inc();
+                   complete(false);
+               }
+            });
+        }
+    }
 }
