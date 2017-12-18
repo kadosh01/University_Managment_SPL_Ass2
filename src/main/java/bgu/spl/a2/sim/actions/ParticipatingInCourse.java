e8f2ec3eee878694a77f84064ced8ad5f459ab2f
Joseph <kadosh01@gmail.com>
2017-12-18 20:26:25 +0200

    20:26

--- src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
+++ src/main/java/bgu/spl/a2/sim/actions/ParticipatingInCourse.java
@@ -2,14 +2,32 @@ package bgu.spl.a2.sim.actions;
 
 import bgu.spl.a2.Action;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Created by Joseph on 17/12/2017.
  */
 public class ParticipatingInCourse extends Action<Boolean> {
-    @Override
-    protected void start() {
-
+    protected String _studentId,_courseName;
+    protected Integer _grade;
+    public ParticipatingInCourse(String studentId,String courseName,Integer grade)
+    {
+        setActionName("Participate In Course");
+        _courseName=courseName;
+        _grade=grade;
+        _studentId=studentId;
     }
 
-    public ParticipatingInCourse(){}
+    @Override
+    protected void start() {
+        List<Action<Boolean>> actions = new LinkedList<>();
+        actions.add(new Action<Boolean>() {
+            @Override
+            protected void start() {
+
+            }
+        });
+    }
+
 }
